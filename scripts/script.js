b2c18b15ecf267100dae3bb90c3a1b4aacc5ff7e
Vitalii <number1pyak@gmail.com>
2023-10-14 17:18:27 +0700

    my cv

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,13 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const body = document.querySelector("body");
   const skills = document.querySelector(".skills__progressbars");
   const progressbars = document.querySelectorAll(".bar__progress span");
-  const skillsPercent = [60, 70, 80, 15, 45];
+  const scrollButton = document.querySelector(".main-block__scroll");
+  const skillsPercent = [80, 87, 83, 75, 65];
+  const works = document.querySelectorAll(".work__item");
+
+  const skillsSection = document.querySelector(".skills");
+  const anchors = document.querySelectorAll(".contacts__string");
+  const sections = document.querySelectorAll("section");
 
   //Activate burger
 
@@ -46,5 +52,50 @@ window.addEventListener("DOMContentLoaded", (event) => {
     }
   }
 
-  Fancybox.bind();
+  var typed = new Typed("#typed-strings", {
+    strings: [
+      "Hello World!^50 My name is V^300italii",
+      "Hello World!^500 I'm Front^300end Developer!",
+      "^500I'm expirienced in Java^150Script, ^350React, ^250Redux",
+    ],
+    smartBackspace: true,
+    typeSpeed: 50,
+    backSpeed: 30,
+    startDelay: 200,
+    backDelay: 700,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: true,
+    cursorChar: "_",
+  });
+
+  works.forEach((work) => {
+    work.addEventListener("mouseenter", (e) => {
+      e.preventDefault();
+      let workExtension = work.querySelector(".work__extension");
+
+      workExtension.classList.add("onShowing");
+    });
+    work.addEventListener("mouseleave", (e) => {
+      e.preventDefault();
+      let workExtension = work.querySelector(".work__extension");
+
+      workExtension.classList.remove("onShowing");
+    });
+  });
+
+  scrollButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    skillsSection.scrollIntoView({ behavior: "smooth" });
+  });
+
+  anchors.forEach((anchor) => {
+    anchor.addEventListener("click", (e) => {
+      sections.forEach((section) => {
+        if (section.classList.contains(e.target.getAttribute("data-anchor"))) {
+          section.scrollIntoView({ behavior: "smooth" });
+        }
+      });
+    });
+  });
 });
